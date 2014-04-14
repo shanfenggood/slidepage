f9931c1942fe4bdc30dfcf59858b8884f3b7ca58
shanfeng <shanfenggood@gmail.com>
2014-04-14 16:54:34 +0800

    init slidepage

---
+++ SlidePageDemo/Class/SlidePageViewController.h
@@ -0,0 +1,16 @@
+//
+//  SlidePageViewController.h
+//  SlidePageDemo
+//
+//  Created by Tim on 14/4/14.
+//  Copyright (c) 2014 shanfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SlidePageViewController : UIViewController
+
+
+
+
+@end
